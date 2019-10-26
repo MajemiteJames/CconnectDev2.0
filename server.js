f2444276e5238ac5b23c0b3406e33cebe7e575ae
MajemiteJames <majemiteokoro@gmail.com>
2019-10-26 14:39:44 +0200

    implemented the user registration route also the jwt validation

--- server.js
+++ server.js
@@ -2,13 +2,27 @@ const express = require('express');
 const connectDB = require('./config/db');
 
 
-const app = express()
+const app = express();
 
-connectDB()
+connectDB();
 
 
-app.get('/', (req, res) => res.send('API Running'))
+// Init Middleware
+app.use(express.json({ extended: false }));
 
-const PORT = process.env.PORT || 5000
+app.get('/', (req, res) => res.send('API Running'));
+
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
+const PORT = process.env.PORT || 5000;
+
+app.all('*', (req, res) => res.status(404).json({
+    status: 404,
+    error: 'Endpoint does not exist',
+}));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
